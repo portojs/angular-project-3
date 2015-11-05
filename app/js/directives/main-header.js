6407cb88fdebf2596f9154c9741702b1c514eb3e
peter <peter.k.nets@gmail.com>
2015-11-05 10:05:51 +0200

    Add nav item highlight on select

--- app/js/directives/main-header.js
+++ app/js/directives/main-header.js
@@ -5,6 +5,15 @@ angular.module('mercCompany')
   .directive('mainHeader', function() {
     return {
       restrict: 'E',
-      templateUrl: 'templates/directives/main-header.html'
+      templateUrl: 'templates/directives/main-header.html',
+      link: function(scope) {
+        scope.link = 1;
+        scope.selectLink = function(linkNr) {
+          scope.link = linkNr;
+        };
+        scope.linkIsSelected = function(linkNr) {
+          return scope.link === linkNr;
+        };
+      }
     }
   });
