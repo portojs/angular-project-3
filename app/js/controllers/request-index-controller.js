88d64df3924e0d78269ad4c97ae7f826ec02d130
peter <peter.k.nets@gmail.com>
2015-11-11 09:21:23 +0200

    Add function to add a new request

--- app/js/controllers/request-index-controller.js
+++ app/js/controllers/request-index-controller.js
@@ -3,5 +3,17 @@
  */
 angular.module('mercCompany')
   .controller('RequestIndexController', function($scope, Requests) {
+    $scope.request = {};
     $scope.requests = Requests.query();
+    $scope.requestsNew = [
+      {
+      category: "Infiltration",
+      title: "In and out",
+      body: "Get some sensitive document from a secret facility in deep mountains."
+      }
+    ];
+    $scope.addRequest = function(){
+      $scope.requestsNew.push($scope.request);
+      $scope.request = {};
+    };
   });
